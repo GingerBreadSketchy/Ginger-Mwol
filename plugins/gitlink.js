7af21b74ecff2841085298c77850d34a11bd4813
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-09-07 11:54:54 +0530

    Update gitlink.js

--- plugins/gitlink.js
+++ plugins/gitlink.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/AVc22LV.jpeg";
+  r_text[0] = "https://i.imgur.com/dCrPuyu.jpeg";
     
     
     var i = Math.floor(1*Math.random())
