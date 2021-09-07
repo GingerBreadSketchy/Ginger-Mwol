68661ed625ae19c878974f8b1bfd527293fedaf9
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-09-07 15:54:04 +0530

    Fidha-Mwol 1.0💝
    Fidha-Mwol 1.0💝

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -41,7 +41,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, dontAddCommandList: tru
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '\n 🙍🏻‍♂️ @' + uye.id.split('@')[0] + '👤';
+                    mesaj += '\n ▢ @' + uye.id.split('@')[0] + '▢';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
