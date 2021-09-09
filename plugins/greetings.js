e8d0fe3e1ff2ffa9f8cf08c8e5fbf642cd5f366b
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-09-09 16:43:03 +0530

    Testing..

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -9,13 +9,13 @@ WhatsAlexa.addCommand({pattern: 'welcome$', fromMe: true, desc: Lang.WELCOME_DES
     if (hg === false) {
         await message.client.sendMessage(message.jid,Lang.NOT_SET_WELCOME,MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data })
     } else {
-        await message.client.sendMessage(message.jid,Lang.WELCOME_ALREADY_SETTED + hg.message + '```',MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+        await message.client.sendMessage(message.jid,Lang.WELCOME_ALREADY_SETTED + hg.message + '```',MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data })
     }
 }));
 
 WhatsAlexa.addCommand({pattern: 'welcome (.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') {
-        return await message.client.sendMessage(message.jid,Lang.NEED_WELCOME_TEXT, {contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data })
+        return await message.client.sendMessage(message.jid,Lang.NEED_WELCOME_TEXT, MessageType.text, { contextInfo: { forwardingScore: 5, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('https://fidhamwol.jpg')}}}});
     } else {
         if (match[1] === 'delete') { await message.client.sendMessage(message.jid,Lang.WELCOME_DELETED,MessageType.text); return await sql.deleteMessage(message.jid, 'welcome'); }
         await sql.setMessage(message.jid, 'welcome', match[1].replace(/#/g, '\n'));
@@ -28,7 +28,7 @@ WhatsAlexa.addCommand({pattern: 'goodbye$', fromMe: true, desc: Lang.GOODBYE_DES
     if (hg === false) {
         await message.client.sendMessage(message.jid,Lang.NOT_SET_GOODBYE,MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data })
     } else {
-        await message.client.sendMessage(message.jid,Lang.GOODBYE_ALREADY_SETTED + hg.message + '```',MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
+        await message.client.sendMessage(message.jid,Lang.GOODBYE_ALREADY_SETTED + hg.message + '```',MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }, quoted: message.data })
     }
 }));
 
