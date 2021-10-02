3b51525ba8c3bb56e30a1b8e1351ef49c79d53fa
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-10-02 17:44:34 +0530

    Create alldown.js

---
+++ plugins/alldown.js
@@ -0,0 +1,34 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('gitlink');
+
+Asena.addCommand({pattern: 'download', fromMe: false, desc: Lang.GL}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/dCrPuyu.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `🎥 ᴅᴏᴡɴʟᴏᴅ ᴄᴏᴍᴍᴀɴᴅs🎥
+┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
+┣𝕾⃝🎥ᴠɪᴅᴇᴏ
+┣𝕾⃝🎥 sᴏɴɢ
+┣𝕾⃝🎥 ᴠɪᴅᴇᴏ
+┣𝕾⃝🎥 ɪɴsᴛᴀ
+┣𝕾⃝🎥 sᴛᴏʀʏ
+┣𝕾⃝🎥 ғʙ
+┣𝕾⃝🎥 ɪᴍɢ
+┣𝕾⃝🎥 ᴡɪᴋɪ
+┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
+`}) 
+
+})); 
