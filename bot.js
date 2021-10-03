1eb7f5d7f8d43649d116787c74ce942c7eee80a2
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-10-03 14:19:45 +0530

    🥳

--- bot.js
+++ bot.js
@@ -266,7 +266,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Fidha Mwol working!')
+            chalk.green.bold('✅️ Fidha Mwol working!')
         );
     });
     
@@ -370,7 +370,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ ғɪᴅͥʜͭᴀᷤ ᴍͫᴡⷷᴏʟ ~______~*' +
+                                await conn.sendMessage(conn.user.jid, '*~_________~ Ғɪᴅʜᴀ Ꮇᴡᴏʟ ~______~*' +
                                     '\n\n*🧞‍♂️ ' + error + '*\n'
                                     , MessageType.text);
                             }
