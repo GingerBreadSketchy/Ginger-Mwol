012a556d5251dde0bcb5d36fb11e9efc5760a16a
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-10-04 13:21:41 +0530

    ✅️

--- plugins/ezysetvar.js
+++ plugins/ezysetvar.js
@@ -11,44 +11,44 @@ const heroku = new Heroku({
 let baseURI = '/apps/' + config.HEROKU.APP_NAME;
 
     Julie.addCommand({ pattern: 'sudo ?(.*)', fromMe: true, desc: 'changes sudo numbers', usage: '.sudo *your number*' }, (async (message, match) => {
-        if (match[1] == '') return await message.sendMessage('NEED A NUMBER')
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ɴᴜᴍʙᴇʀ')
         await heroku.patch(baseURI + '/config-vars', {
             body: {
                 ['SUDO']: match[1]
             }
         });
-        await message.sendMessage("NEW SUDO UPDATED")
+        await message.sendMessage("ɴᴇᴡ sᴜᴅᴏ ᴜᴘᴅᴀᴛᴇᴅ")
     }));
 
-    Julie.addCommand({ pattern: 'caption ?(.*)', fromMe: true, desc: 'changes all captions', usage: '.caption *Made by JulieMwol*' }, (async (message, match) => {
-        if (match[1] == '') return await message.sendMessage('NEED cA CAPTION')
+    Julie.addCommand({ pattern: 'caption ?(.*)', fromMe: true, desc: 'changes all captions', usage: '.caption Made by FidhaMwol' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ᴄᴀᴘᴛɪᴏɴ ')
         await heroku.patch(baseURI + '/config-vars', {
             body: {
                 ['ALL_CAPTION']: match[1]
             }
         });
-        await message.sendMessage("NEW CAPTION UPDATED")
+        await message.sendMessage("ɴᴇᴡ ᴄᴀᴘᴛɪᴏɴ ᴜᴘᴅᴀᴛᴇᴅ ")
     }));
 
     Julie.addCommand({ pattern: 'handlers ?(.*)', fromMe: true, desc: 'changes handlers', usage: '.handler ^[.!] ' }, (async (message, match) => {
-        if (match[1] == '') return await message.sendMessage('NEED A CAPTION')
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ʜᴀɴᴅʟᴇʀ')
         await heroku.patch(baseURI + '/config-vars', {
             body: {
                 ['ALL_CAPTION']: match[1]
             }
         });
-        await message.sendMessage("NEW HANDLER UPDATED")
+        await message.sendMessage("ɴᴇᴡ ʜᴀɴᴅʟᴇʀ ᴜᴘᴅᴀᴛᴇᴅ ")
     }));
 
 
-    Julie.addCommand({ pattern: 'botname ?(.*)', fromMe: true, desc: 'change your bot name', usage: '.botname *name* ' }, (async (message, match) => {
-        if (match[1] == '') return await message.sendMessage('TYPE YOUR NEW BOT NAME')
+    Julie.addCommand({ pattern: 'botname ?(.*)', fromMe: true, desc: 'change your bot name', usage: '.botname name ' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ᴛʏᴘᴇ ʏᴏᴜʀ ɴᴇᴡ ʙᴏᴛ ɴᴀᴍᴇ')
         await heroku.patch(baseURI + '/config-vars', {
             body: {
                 ['BOT_NAME']: match[1]
             }
         });
-        await message.sendMessage("NEW BOT NAME UPDATED")
+        await message.sendMessage("ɴᴇᴡ ʙᴏᴛ ɴᴀᴍᴇ ᴜᴘᴅᴀᴛᴇᴅ ")
     }));
 
 Julie.addCommand({ pattern: 'theri  ?(.*)', fromMe: true, desc: 'change your theri commands', usage: '.theri command,command' }, (async (message, match) => {
@@ -58,7 +58,7 @@ Julie.addCommand({ pattern: 'theri  ?(.*)', fromMe: true, desc: 'change your the
                 ['THERI_LIST']: match[1]
             }
         });
-        await message.sendMessage("THERI LIST UPDATED")
+        await message.sendMessage("ᴛʜᴇʀɪ ʟɪsᴛ ᴜᴘᴅᴀᴛᴇᴅ ")
     }));
 
 
