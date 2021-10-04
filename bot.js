9549d0ed59b2729918134e249748b81bca7da9b4
Hyper-sir <89091746+Hypersir@users.noreply.github.com>
2021-10-04 13:52:01 +0530

    ✅️

--- bot.js
+++ bot.js
@@ -370,7 +370,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ Ғɪᴅʜᴀ Ꮇᴡᴏʟ ~______~*' +
+                                await conn.sendMessage(conn.user.jid, '*~______~ Ғɪᴅʜᴀ Ꮇᴡᴏʟ ~______~*' +
                                     '\n\n*🧞‍♂️ ' + error + '*\n'
                                     , MessageType.text);
                             }
